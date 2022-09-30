c3fdc5968315cc1854e484acbfca43b4390e352b
zijiancogito <345981440@qq.com>
2022-09-30 19:46:16 +0800

    finish building variable and type map

--- src/call/decompiler_call.c
+++ src/call/decompiler_call.c
@@ -27,11 +27,16 @@ typedef struct {
     int num_of_args;
 }Function;
 
-struct variables {
-	int id;
-	char * name;
+typedef struct {
+	char * type_type;
+	char * type_name;
+}Type;
+
+typedef struct{
+	char id[20];
+	Type *name;
 	UT_hash_handle hh;
-};
+}VarMap;
 
 char * get_content_in_source(int start, int end, const char *source) {
     char * content = (char*)malloc((end-start+1)*sizeof(char));
@@ -84,12 +89,60 @@ int parse_call_expression(TSNode node, Argument ** arguments, const char *source
     return child_count;
 }
 
-void build_variable_map(NodeList all_var_nodes, const char *source){
+void parse_declaration(TSNode node, VarMap ** var_map, const char * source) {
+	TSTreeCursor cursor = ts_tree_cursor_new(node);
+	bool child = ts_tree_cursor_goto_first_child(&cursor);
+	TSNode type_node = ts_tree_cursor_current_node(&cursor);
+	const char * type_type = ts_node_type(type_node);
+	child = ts_tree_cursor_goto_next_sibling(&cursor);
+	TSNode identifier_node = ts_tree_cursor_current_node(&cursor);
+
+	Type *tp = (Type*)malloc(sizeof(Type));
+	tp->type_type = (char*)malloc(strlen(type_type)*sizeof(char));
+	strcpy(tp->type_type, type_type);
+	int type_start = ts_node_start_point(type_node).column;
+	int type_end = ts_node_end_point(type_node).column;
+	tp->type_name = get_content_in_source(type_start, type_end, source);
+
+	int id_start = ts_node_start_point(identifier_node).column;
+	int id_end = ts_node_end_point(identifier_node).column;
+	char id[20];
+        strcpy(id, get_content_in_source(id_start, id_end, source));
+	
+	VarMap *s;
+	s = (VarMap *)malloc(sizeof(*s));
+	strcpy(s->id, id);
+	s->name = tp;
+	HASH_ADD_STR(*var_map, id, s);
+}
+
+void build_variable_map(NodeList all_var_nodes, const char *source, VarMap **var_map){
 	/* Build the map of the variables and its type.
 	 *
 	 */
+	Node *tmp = all_var_nodes.head;
+	for (int i = 0; i < all_var_nodes.listLen; i++) {
+		tmp = tmp->next;
+		printf("%s\n%s\n", ts_node_string(tmp->data), ts_node_type(tmp->data));	
+		int start = ts_node_start_point(tmp->data).column;
+		int end = ts_node_end_point(tmp->data).column;
+		char * src = get_content_in_source(start, end, source);
+		printf("%s\n", src);
+		printf("\n");
+		parse_declaration(tmp->data, var_map, source); 
+	}
+}
 
-		
+char * get_variable_name(const char * var) {
+	char * result = (char*)malloc(strlen(var));
+	int cnt = 0;
+	for (int i = 0; i < strlen(var); i++) {
+		if (var[i] != '&' && var[i] != '*') {
+			result[cnt] = var[i];
+			cnt++;
+		}
+	}
+	return result;
 }
 
 void process(enum Decompiler decompiler, const char * filename) {
@@ -110,11 +163,11 @@ void process(enum Decompiler decompiler, const char * filename) {
     // Save filtered nodes in all_nodes
     NodeList all_call_nodes, all_var_nodes;
     parse_decompiler_output(tree, source, "call_expression", &all_call_nodes);
-    parse_decompiler_output(tree, source, "variable_declaration", &all_var_nodes);
+    parse_decompiler_output(tree, source, "declaration", &all_var_nodes);
     
     // Build map of variables and its type
-    Node *var_tmp = all_var_nodes.head;
-
+    VarMap *var_map = NULL;
+    build_variable_map(all_var_nodes, source, &var_map);
 
     // Parse all_call_nodes
     Node *tmp = all_call_nodes.head;
@@ -132,6 +185,12 @@ void process(enum Decompiler decompiler, const char * filename) {
 		if(is_other_symbol(functions[i].args[j].type) == 0) {
 
 			printf("%s %s\n", functions[i].args[j].type, functions[i].args[j].content);
+			VarMap *s = NULL;
+			char * var_name = get_variable_name(functions[i].args[j].content);
+			HASH_FIND_STR(var_map, var_name, s);
+			// assert(s!=NULL);
+			if (s != NULL)
+				printf("%s\n", s->name->type_name);
 		}
 	}
 	printf("\n");
