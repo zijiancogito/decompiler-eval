86743675c5fd0828512d530d1e6c1c97f54a0740
zijiancogito <345981440@qq.com>
2023-03-21 16:44:52 +0800

    update

--- src/readability/var/var.py
+++ src/readability/var/var.py
@@ -12,22 +12,14 @@ C = Language('build/my-language.so', 'c')
 
 c_parser = Parser()
 c_parser.set_language(C)
-code = """
-int main(){
-    int a, b;
-    printf("%d", a);
-    return b;
-    }
-"""
 
-def var_filter(node):
-    if node.type == "declaration":
+def var_filter(node, parent_type):
+    if node.type == "identifier" and parent_type == "declaration":
         return True
     return False
 
 def copy_content(start, end, code):
     tmp = [list(code) for i in code.split('\n')]
-    print(tmp)
     content = ''.join(tmp[start[0]][start[1]:end[1]])
 
     return content
@@ -35,55 +27,69 @@ def copy_content(start, end, code):
 def get_content(code, node):
     start = node.start_point
     end = node.end_point
-    print(start)
-    print(end)
     content = copy_content(start, end, code)
-    print(content)
+    return content
             
-def make_move(cursor, move, ):
-    print(cursor.node.type)
-    get_content(code, cursor.node)
+def make_move(cursor, move, parent_type):
+    current_node = cursor.node
+    nodes = []
+    if var_filter(cursor.node, parent_type):
+        nodes.append(cursor.node)
     if move == 'down':
         if cursor.goto_first_child():
-            make_move(cursor, 'down')
+            nodes.extend(make_move(cursor, 'down', current_node.type))
         elif cursor.goto_next_sibling():
-            make_move(cursor, 'right')
+            nodes.extend(make_move(cursor, 'right', parent_type))
         elif cursor.goto_parent():
-            make_move(cursor, 'up')
+            nodes.extend(make_move(cursor, 'up', parent_type))
     elif move == 'right':
         if cursor.goto_first_child():
-            make_move(cursor, 'down')
+            nodes.extend(make_move(cursor, 'down', current_node.type))
         elif cursor.goto_next_sibling():
-            make_move(cursor, 'right')
+            nodes.extend(make_move(cursor, 'right', parent_type))
         elif cursor.goto_parent():
-            make_move(cursor, 'up')
+            nodes.extend(make_move(cursor, 'up', parent_type))
     elif move == 'up':
         if cursor.goto_next_sibling():
-            make_move(cursor, 'right')
+            nodes.extend(make_move(cursor, 'right', parent_type))
         elif cursor.goto_parent():
-            make_move(cursor, 'up')
+            nodes.extend(make_move(cursor, 'up', parent_type))
+    return nodes
 
 def preprocess_code(code):
     code = re.sub('[\s]+', ' ', code.strip())
-    print(code)
     return code
 
-def parse_code(code):
-    code = preprocess_code(code)
+def get_all_vars(code):
     tree = c_parser.parse(bytes(code, "utf8"))
     cursor = tree.walk()
-    all_nodes = []
-    make_move(cursor, 'down', all_nodes)
+    all_nodes = make_move(cursor, 'down', 'root')
+    vars = [get_content(code, n) for n in all_nodes]
+    return vars
 
+def read_code_from_file(code_file):
+    code = None
+    with open(code_file, 'r') as f:
+        code = f.read()
+    code = preprocess_code(code)
+    return code
 
 def test():
-    global code
+    code = """
+    int main(){
+        int a, b;
+        printf("%d", a);
+        return b;
+        }
+    """
     code = preprocess_code(code)
     tree = c_parser.parse(bytes(code, "utf8"))
     root_node = tree.root_node
 
     cursor = tree.walk()
-    make_move(cursor, 'down')
+    all_nodes = make_move(cursor, 'down', cursor.node.type)
+    for node in all_nodes:
+        print(get_content(code, node))
 
 if __name__ == '__main__':
     test()
---
+++ src/readability/var/var_count.py
@@ -0,0 +1,6 @@
+import sys
+
+from var import *
+
+def analyze_var_count(src, function):
+
