39af5585c436d28a68fc3a4225a2d58524481b66
zijiancogito <345981440@qq.com>
2023-04-12 14:39:12 +0800

    update

--- analyze/ex1/concolic_analyze.py
+++ analyze/ex1/concolic_analyze.py
@@ -1,28 +1,77 @@
+import sys
+
+import os
+import re
+import numpy as np
 
 
 
-root = '/home/eval/DF/se/'
+dec_root = '/home/eval/DF/se/de'
+ir_root = '/home/eval/DF/se/ir'
 compilers = ['clang', 'gcc']
-decompilers = ['angr', 'BinaryNinja', 'Boomerang', 'dewolf', 'Ghidra', 'ida', 'RecStudio', 'Reko', 'Relyze', 'RetDec', 'Snowman']
+decompilers = ['angr', 'BinaryNinja', 'Ghidra', 'ida', 'RetDec']
+# decompilers = ['Ghidra',  'ida', 'RetDec']
 options = ['o0', 'o1', 'o2', 'o3', 'os']
-match_algos = ['fullmatch', 'feature', 'concrete']
 
 
-def analyze(compiler, decompiler):
-    dir = os.path.join(root, compiler, decompiler)
-    for opt in options:
-        dec_dir = os.path.join(dir, opt)
-        ir_dir = os.path.join(root, 'ir', opt)
-        for algo in match_algos:
-            if algo == 'fullmatch' or algo == 'feature':
-                batch_compare(ir_dir, dec_dir, match_algo)
-            elif algo == 'concrete':
-                ce.batch_compare(ir_dir, dec_dir)
 
-def analyze_all():
+def get_func_acc(dec_se, ir_se, option):
+    if option == 'feature' or option == 'fullmatch':
+        sys.path.append('../../src/exp_tree/')
+        from compare import tree_sta
+        p, r = tree_sta(ir_se, dec_se, option)
+        return p, r
+    elif option == 'concrete':
+        sys.path.append('../../src/exp_tree')
+        from concrete_execution import concrete_sta
+        p, r = concrete_sta(ir_se, dec_se)
+        return p, r
+    return None, None
+
+def analyze(de_dir, ir_dir, option):
+    de_sub_dirs = os.listdir(de_dir)
+    ps, rs = [], []
+    for sub_dir in de_sub_dirs:
+        ir_sub_dir = os.path.join(ir_dir, sub_dir)
+        if not os.path.exists(ir_sub_dir):
+            continue
+        de_sub_dir = os.path.join(de_dir, sub_dir)
+        funcs = os.listdir(de_sub_dir)
+        for func in funcs:
+            bname = func.split('.')[0]
+            if not re.match('func[0-9]', bname):
+                continue
+            ir_json = os.path.join(ir_sub_dir, func)
+            if not os.path.exists(ir_json):
+                continue
+            de_json = os.path.join(de_sub_dir, func)
+            p, r = get_func_acc(de_json, ir_json, option)
+            if p != None:
+                ps.append(p)
+                rs.append(r)
+
+    ps_avg = 0.0
+    rs_avg = 0.0
+    if len(ps) != 0:
+        ps_avg = np.mean(ps)
+    if len(rs) != 0:
+        rs_avg = np.mean(rs)
+    return round(ps_avg, 2), round(rs_avg, 2)
+    
+def analyze_all(option):
     for compiler in compilers:
+        print(f"---------------------------{compiler}-------------------------------")
+        print("Decompiler\tO0       \tO1       \tO2       \tO3       \tOs       ")
         for decompiler in decompilers:
-            analyze(compiler, decompiler)
+            print("{0:15}".format(decompiler), end='\t')
+            for opt_level in options:
+                de_dir = os.path.join(dec_root, compiler, decompiler, opt_level)
+                ir_dir = os.path.join(ir_root, opt_level)
+                ps, rs = analyze(de_dir, ir_dir, option)
+                print(f"{ps} {rs}", end='\t')
+            print()
+        print()
+
 
 if __name__ == '__main__':
-    analyze_all()
+    analyze_all('feature')
