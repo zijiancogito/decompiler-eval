228d3e383fbc41520eeba10947d26fd840b77d46
zijiancogito <345981440@qq.com>
2023-04-21 15:48:47 +0800

    ERROR

---
+++ new_src/data_process/raw/ir/README.md
@@ -0,0 +1,7 @@
+# README
+
+Run `run.sh` to check if there are IF, POISON, ATTR instructions in ir files and remove them from dataset.
+These instructions may lead to IR parsing or execution failed.
+Removed files will be save to `trash` dir and there are log file in corresponding dir.
+
+If you want to check if the last step succeed, run `check.sh` 
---
+++ new_src/data_process/raw/ir/df2_check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IR_DIR=/home/eval/DF2/process/ir
+IF_MOVE_DIR=/home/eval/DF2/trash/ir/ir_if
+POISON_MOVE_DIR=/home/eval/DF2/trash/ir/ir_poison
+rm -r $POISON_MOVE_DIR
+rm -r $IF_MOVE_DIR
+
+python3 check_attr.py -i $IR_DIR
+echo "\n"
+python3 check_poison.py -i $IR_DIR -o $POISON_MOVE_DIR 
+echo "\n"
+python3 check_if.py -i $IR_DIR -o $IF_MOVE_DIR 
+
+echo "checking result"
+
+python3 check.py -i $IR_DIR -o IF ATTR POISON
