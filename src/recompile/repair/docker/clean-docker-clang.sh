e7e1192347e3e95f27713036a1e0d4fce6c920e6
zijiancogito <345981440@qq.com>
2023-07-21 11:21:04 +0000

    update

--- src/recompile/repair/docker/clean-docker-clang.sh
+++ src/recompile/repair/docker/clean-docker-clang.sh
@@ -14,25 +14,22 @@ TIMEOUT_DIR=$DATA_DIR/recompile/round-2/timeout
 mkdir -p $FIXED_DIR $UNFIXED_DIR $TIMEOUT_DIR
 
 create_container(){
-	name=$1_$2_$3
-	# podman run -d -p $5:8000 --name $name-transfix localhost/transfix:v0
-	# sleep 5s
-	# podman attach $name-transfix
-	# sleep 5s
-	podman run -d -p $4:4444 -p $5:7900 --shm-size="2g" --name $name-webdriver selenium/standalone-firefox:latest
+	name=$1_$2_$3-webdriver
+	# podman run -d -p $4:4444 -p $5:7900 --shm-size="2g" --name $name selenium/standalone-firefox:latest
 	# podman start $name
+	podman stop $name
 	sleep 5s
-	python3 drrepair.py -d $DE_DIR -f $FIXED_DIR -u $UNFIXED_DIR -t $TIMEOUT_DIR -D $3 -C $1 -O $2 -p $4 -P $5 &
+	podman rm $name
+	# sleep 5s
+	# python3 drrepair.py -d $DE_DIR -f $FIXED_DIR -u $UNFIXED_DIR -t $TIMEOUT_DIR -D $3 -C $1 -O $2 -p $4 &
 }
 
 wdport=4000
 hubport=7000
-webport=8001
-
 for compiler in ${COMPILERS[*]}; do
 	for opt in ${OPTIMIZATIONS[*]}; do
 		for decompiler in ${DECOMPILERS[*]}; do
-			create_container ${compiler} ${opt} ${decompiler} ${wdport} ${hubport} ${webport}
+			create_container ${compiler} ${opt} ${decompiler} ${wdport} ${hubport}
 			wdport=$((${wdport}+1))
 			hubport=$((${hubport}+1))
 		done
