cde2110e24d367d57c918616cc179e04d70e17d6
zijiancogito <345981440@qq.com>
2023-07-22 23:02:51 +0800

    update

---
+++ src/recompile/analyze/repair/repair-results.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ROOT_DIR=/home/caoy/deceval/data/POJ/recompile/round-2
+FIXED_DIR=$ROOT_DIR/fixed
+UNFIXED_DIR=$ROOT_DIR/unfixed
+TIMEOUT_DIR=$TIMEOUT_DIR/timeout
+
+COMPILERS="clang gcc"
+OPTIMIZATIONS="o0 o1 o2 o3 os"
+DECOMPILERS="BinaryNinja Ghidra Hex-Rays RetDec"
+
+python3 repair-result.py -f $FIXED_DIR -u $UNFIXED_DIR -t $TIMEOUT_DIR -D $DECOMPILERS -C $COMPILERS -O $OPTIMIZATIONS
