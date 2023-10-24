86d1f83390e30410ddebf9f0b55d340e082fc99c
zijiancogito <345981440@qq.com>
2023-10-24 21:51:19 +0800

    update src

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # decompiler-eval
 
-## USAGE
+## INSTALL
 
 ```
 apt-get update
 apt-get install python3 python3-pip
+```
+
+## USAGE
+
+### Dataset
+
+1. Random generate data
+
+```
+cd src/dataset/rgd
+
+python3 gen.py -n 1000 -c ./config -s ./dsmith/source.py -o $RGD/raw/src/
+python3 make.py ir --opt O0 O1 O2 O3 Os --src $RGD/raw/src/ --out $RGD/raw/ir/ --log $RGD/log/make.log 
+python3 make.py bin --opt O0 O1 O2 O3 Os --src $RGD/raw/src/ --out $RGD/raw/bin/ --log $RGD/log/make.log
+
 ```
--- src/dataset/rgd/make.py
+++ src/dataset/rgd/make.py
@@ -4,19 +4,93 @@ import csv
 import subprocess
 import argparse
 from tqdm import tqdm
+import logging
 
-err_list = []
 
-
-def make_ir(opt_level, src_path, save_to):
+def make_ir(config, src_path, save_to, log_file):
+    opt = config["opt"]
+    cflags = config["cflags"]
     c_list = os.listdir(src_path)
     cnt = 0
+    err_cnt = 0
     total = 0
     for c in tqdm(c_list):
         total += 1
         c_path = os.path.join(src_path, c)
         c_name = os.path.splitext(c)[0]
         ll_path = os.path.join(save_to, c_name+'.ll')
-        cmd = f'clang -{opt_level.capitalize()} -emit-llvm -fno-inline-functions -fno-default-inline -fno-inline-functions-called-once ' + c_path + ' -S -o ' + ll_path
+        cmd = f"clang -emit-llvm -{opt} {' '.join(cflags)} c_path -S -o {ll_path}"
         with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
-            stdout, stderr = 
+            stdout, stderr = p.communicate()
+            stderr = stderr.decode('ISO-8859-1')
+            if ('error' in stderr):
+                logging.error(f"IR ERROR: {opt} {c_path}")
+                err_cnt += 1
+            else:
+                logging.info(f"IR PASS: {opt} {c_path}")
+                cnt += 1
+    print(f"{cnt}/{total} {opt} IR compilation complete.")
+
+def make_bin(config, src_path, save_to, log_file):
+    opt = config["opt"]
+    cflags = config["cflags"]
+    cc = config["cc"]
+    c_list = os.listdir(src_path)
+    cnt = 0
+    err_cnt = 0
+    total = 0
+    for c in tqdm(c_list):
+        total += 1
+        c_path = os.path.join(src_path, c)
+        c_name = os.path.splitext(c)[0]
+        binary_path = os.path.join(save_to, c_name)
+        cmd = f"{cc} -{opt} {' '.join(cflags)} {c_path} -o {binary_path}"
+        with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
+            stdout, stderr = p.communicate()
+            stderr = stderr.decode('ISO-8859-1')
+            if 'error' in stderr:
+                logging.error(f"BIN ERROR: {cc} {opt} {c_path}")
+                err_cnt += 1
+            else:
+                logging.info(f"BIN PASS: {cc} {opt} {c_path}")
+                cnt += 1
+    print(f"{cnt}/{total} {opt} BIN compilation complete.")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Batch compilation program for random data set.')
+    parser.add_argument('--opt', nargs='+', required=True, help="Compiler optimization level.")
+    parser.add_argument('--src', type=str, required=True, help='Path to source code')
+    parser.add_argument('--out', type=str, required=True, help='Path to save results')
+    parser.add_argument('--log', type=str, required=True, help='Path to logging')
+
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    ir_parser = subparsers.add_parser('ir', help='IR')
+
+    bin_parser = subparsers.add_parser('bin', help='BIN')
+    bin_parser.add_argument('--cc', type=str, default='gcc', required=False, help='Path to compiler')
+    args = parser.parse_args()
+
+    logging.basicConfig(filename=args.log, encoding='utf-8', level=logging.DEBUG)
+
+    cflags = ['-fno-inline-functions',
+              '-fno-default-inline',
+              '-fno-inline-functions-called-once'
+    ]
+
+
+    if args.command == 'ir':
+        for opt in args.opt:
+            config = {"cflags": cflags, "opt": opt,}
+            src_dir = os.path.join(args.src, opt)
+            out_dir = os.path.join(args.src, opt)
+            make_ir(config, src_dir, out_dir, args.log)
+    elif args.command == 'bin':
+        for opt in args.opt:
+            config = {"cflags": cflags, "opt": opt, "cc": args.cc}
+            src_dir = os.path.join(args.src, opt)
+            out_dir = os.path.join(args.src, opt)
+            make_bin(config, src_dir, out_dir, args.log)
+
+    
